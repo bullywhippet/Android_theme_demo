ca9099cf1651947e801f99e13220abcadc0fa908
Kelly Walters <kellywdev@gmail.com>
2018-03-15 22:13:08 -0500

    Initial commit

---
+++ app/src/main/java/com/kellydwalters/demotheme/MainActivity.java
@@ -0,0 +1,30 @@
+package com.kellydwalters.demotheme;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+        // TO change the theme go to res > values > styles to change code
+        // or
+        // Tools > Android > Theme Editor
+
+        // To change the launcher Icon right click res folder, new>image asset
+    }
+
+
+
+}
